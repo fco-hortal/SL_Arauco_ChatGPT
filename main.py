c00b00c26f812900eaac023b7c9c0ceb70b51f62
Marco <mtaguero@uc.cl>
2023-11-07 01:23:13 -0300

    feat: Main funcionando

--- main.py
+++ main.py
@@ -5,19 +5,16 @@ from dotenv import load_dotenv
 # Carga las variables de entorno desde el archivo .env
 load_dotenv()
 # Configura tu clave de API de OpenAI
-openai.api_key = os.getenv("OPENAI_API_KEY") 
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# Example OpenAI Python library request
-MODEL = "gpt-3.5-turbo"
-response = openai.completions.create(
-    model=MODEL,
-    prompt=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Knock knock."},
-        {"role": "assistant", "content": "Who's there?"},
-        {"role": "user", "content": "Orange."},
-    ],
-    temperature=0,
+client = openai.OpenAI()
+
+completation = client.completions.create(
+    model="gpt-3.5-turbo-instruct",
+    prompt="Hey. How are you?",
+    max_tokens=7,
+    temperature=0
 )
 
-response
+answer = completation.choices[0].text
+print(answer)
