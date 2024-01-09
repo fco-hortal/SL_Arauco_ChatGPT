47b5a46924772036ed90f6173a2e1bfd0c512a32
fco-hortal <francisco.hortal@uc.cl>
2024-01-08 23:06:08 -0300

    borrar innecesario y areglar bot en front

--- bot_front.py
+++ bot_front.py
@@ -1,55 +1,60 @@
+from langchain.memory import ConversationBufferWindowMemory, ConversationSummaryMemory, ConversationKGMemory, CombinedMemory
 from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent as pd_agent
-from langchain.llms import OpenAI
 from langchain.agents.agent_types import AgentType
 from langchain.chat_models import ChatOpenAI
+from utils import convert_dtypes, CONTEXT, FORMAT_INSTRUCTIONS
+from langchain.llms import OpenAI
 from qvd import qvd_reader
 import pandas as pd
+from datetime import datetime
+import locale
+# pip install "openai<1.0.0"
 import openai
 from dotenv import load_dotenv
 import os
 import time
+
+# Localidad
+locale.setlocale(locale.LC_TIME, 'es_ES.utf8')
 
 # OpenAI Api Key
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 openai.api_key = api_key
 
-
-def convert_dtypes(df):
-    """
-    Conversión numérica
-    """
-    cols = df.columns
-    for c in cols:
-        try:
-            df[c] = pd.to_numeric(df[c])
-        except:
-            pass
-
-
 # Import the QVD file
-pv = qvd_reader.read('data/Precio_venta.qvd')
 vp = qvd_reader.read('data/Volumen_Prod.qvd')
-convert_dtypes(pv)
+pv = qvd_reader.read('data/Precio_venta.qvd')
 convert_dtypes(vp)
+convert_dtypes(pv)
 
-agent = pd_agent(
-    OpenAI(temperature=0),
-    [pv, vp],
-    verbose=True
+# Create agent
+llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613")
+chat = ChatOpenAI(
+    model_name='gpt-3.5-turbo-0613',
+    temperature=0,
+    openai_api_key=api_key,
 )
+
+chat_history_buffer = ConversationBufferWindowMemory(
+    k=5, memory_key="chat_history_buffer", input_key="input")
+chat_history_summary = ConversationSummaryMemory(
+    llm=llm, memory_key="chat_history_summary", input_key="input")
+chat_history_KG = ConversationKGMemory(
+    llm=llm, memory_key="chat_history_KG", input_key="input")
+memory = CombinedMemory(
+    memories=[chat_history_buffer, chat_history_summary, chat_history_KG])
+
+agent = pd_agent(chat, [vp, pv], verbose=True,
+                 #agent_executor_kwargs={"memory": memory},
+                 #input_variables=['df_head', 'input', 'agent_scratchpad', 'chat_history_buffer', 'chat_history_summary', 'chat_history_KG']
+                 )
 
 
 def consulta(input_usuario):
     tiempo_inicial = time.time()
-    context = """
-    Eres un chatbot que responde preguntas sobre el sistema financiero de Arauco.
-    Para ello te entrego dos bases de datos: volúmen de productos y ventas de productos.
-    A continuación te haré una pregunta sobre estas bases de datos. Quiero que a tu
-    respuesta le añadas la consulta que haces a Pandas para encontrar la información solicitada.
-    La pregunta es la siguiente:\n
-    """
-    output = agent.run(context + input_usuario)
+    output = agent.run(CONTEXT +
+                       input_usuario + FORMAT_INSTRUCTIONS)
     tiempo_final = time.time()
     print(f"Tiempo de ejecución: {tiempo_final - tiempo_inicial}")
     return (output)
